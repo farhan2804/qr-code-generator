223ac38bf1a6e4a975a942493b1cb7682a935493
farhan <el3317.farhanmahmood@gmail.com>
2023-08-10 21:38:39 +0530

    config update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   const config = {
     plugins: [react()],
     base: '/qr-code-generator/',
